5b5fb1d4cfd5b07db95aea5a13778b0f42402785
Debayo Tomisin <debayotomisin07@gmail.com>
2021-03-11 09:47:17 -0800

    App js and App css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
-import './assets/css/style.css';
+import "./App.css"
 
 function App() {
   return (
